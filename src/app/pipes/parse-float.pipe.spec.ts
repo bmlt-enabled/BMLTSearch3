696755a34b4a712285a346c134dc7ee73b9b14d7
paulnagle <paulnagle@users.noreply.github.com>
2022-02-05 12:59:33 +0000

    deploy: 1804e4d1e0650038a90b18bd7befd8cd2d087317

--- src/app/pipes/parse-float.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ParseFloatPipe } from './parse-float.pipe';
-
-describe('ParseFloatPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ParseFloatPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/app/pipes/tidy-delimiter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TidyDelimiterPipe } from './tidy-delimiter.pipe';
-
-describe('TidyDelimiterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TidyDelimiterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
